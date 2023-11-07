0f25b2c258e96956910d677f1a98235c7431c614
kawakaming11 <ttkkwkm3000@gmail.com>
2023-11-07 22:05:34 +0900

    create_init_prompt修正

--- create_init_prompt.py
+++ create_init_prompt.py
@@ -10,6 +10,10 @@ api_key = os.environ['OPENAI_API_KEY_V']
 # argument "image_path": string
 # return "encoded_string": string
 def encode_image(image_path):
+    # jpg以外は受け付けない
+    if not image_path.endswith(".jpg") and not image_path.endswith(".jpeg") and not image_path.endswith(".JPG") and not image_path.endswith(".JPEG"):
+        raise Exception("The image must be jpg.")
+    
     with open(image_path, "rb") as image_file:
         encoded_string = base64.b64encode(image_file.read()).decode('utf-8')
         return encoded_string
@@ -17,7 +21,12 @@ def encode_image(image_path):
 # argument "image_path": string
 # return "prompt": string
 def create_init_prompt(image_path):
-    base64_image = encode_image(image_path)
+    # 画像のパスがURLの場合はそのまま使う
+    image_url = image_path
+    # 画像のパスがローカルの場合はbase64にエンコードする
+    if not image_path.startswith("http"):
+        base64_image = encode_image(image_path)
+        image_url = f"data:image/jpeg;base64,{base64_image}"
 
     headers = {
         "Content-Type": "application/json",
@@ -37,7 +46,7 @@ def create_init_prompt(image_path):
                     {
                         "type": "image_url",
                         "image_url": {
-                            "url": f"data:image/jpeg;base64,{base64_image}"
+                            "url": image_url
                         }
                     }
                 ]
@@ -48,6 +57,8 @@ def create_init_prompt(image_path):
     }
 
     response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
+    response.raise_for_status()
+
     # print(response.json())
     prompt = response.json().get("choices")[0].get("message").get("content")
     # 両端のクォーテーションマークを削除
@@ -56,5 +67,5 @@ def create_init_prompt(image_path):
     return prompt
 
 # テスト
-prompt = create_init_prompt("data/origin/origin_2.jpg")
+prompt = create_init_prompt("data/origin/origin_1.jpg")
 print(prompt)
