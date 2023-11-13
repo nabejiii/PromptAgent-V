ed631444c9675e08bc3a27bcba3e21c6606c4a01
Kenta Watanabe <kenta2840006@g.ecc.u-tokyo.ac.jp>
2023-11-13 15:35:13 +0900

    add mock

---
+++ mock.py
@@ -0,0 +1,16 @@
+import random
+
+def image_val(img_path1, img_path2):
+    return random.random()
+
+def save_image(image_path, image_url):
+    pass
+
+def create_image(prompt):
+    return "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"
+
+def improve_prompt(origin_image, image_path, prompt):
+    return prompt + "improved"
+
+def create_init_prompt(origin_image):
+    return "init_prompt"
