08cf66a41f73903b7e34b4454087f8387a82a35f
Kenta Watanabe <kenta2840006@g.ecc.u-tokyo.ac.jp>
2023-11-14 15:07:58 +0900

    実装

--- mock.py
+++ mock.py
@@ -10,7 +10,7 @@ def create_image(prompt):
     return "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"
 
 def improve_prompt(origin_image, image_path, prompt):
-    return prompt + "improved"
+    return "diff", prompt + "improved"
 
 def create_init_prompt(origin_image):
     return "init_prompt"
