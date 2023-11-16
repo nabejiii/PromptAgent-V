949b07f3dbb65a73b2a889bb5f56873568b4a3b8
kawakaming11 <ttkkwkm3000@gmail.com>
2023-11-16 13:48:49 +0900

    improvement修正

---
+++ calc_diff_2.py
@@ -0,0 +1,59 @@
+import cv2
+import os
+import image_similarity_measures
+from image_similarity_measures.quality_metrics import rmse, ssim, sre
+
+img = "data/image_1/origin_1.jpg"
+
+test_img = cv2.imread(img)
+
+ssim_measures = {}
+rmse_measures = {}
+sre_measures = {}
+
+scale_percent = 100 # percent of original img size
+width = int(test_img.shape[1] * scale_percent / 100)
+height = int(test_img.shape[0] * scale_percent / 100)
+dim = (width, height)
+
+data_dir = './data/samples'
+
+# for file in os.listdir(data_dir):
+# 	img_path = os.path.join(data_dir, file)
+# 	data_img = cv2.imread(img_path)
+# 	resized_img = cv2.resize(data_img, dim, interpolation = cv2.INTER_AREA)
+# 	ssim_measures[img_path]= ssim(test_img, resized_img)
+# 	rmse_measures[img_path]= rmse(test_img, resized_img)
+# 	sre_measures[img_path]= sre(test_img, resized_img)
+
+img_path = "data/samples/sixth_modified1.jpg"
+data_img = cv2.imread(img_path)
+resized_img = cv2.resize(data_img, dim, interpolation = cv2.INTER_AREA)
+ssim_measures[img_path]= ssim(test_img, resized_img)
+rmse_measures[img_path]= rmse(test_img, resized_img)
+sre_measures[img_path]= sre(test_img, resized_img)
+
+def calc_closest_val(dict, checkMax):
+	result = {}
+	closest = 0
+	if checkMax:
+		closest = max(dict.values())
+	else:
+		closest = min(dict.values())
+		
+	for key, value in dict.items():
+		print("The difference between ", key ," and the original image is : \n", value)
+		if (value == closest):
+			result[key] = value
+
+	print("The closest value: ", closest)	    
+	print("######################################################################")
+	return result
+    
+ssim = calc_closest_val(ssim_measures, True)
+rmse = calc_closest_val(rmse_measures, False)
+sre = calc_closest_val(sre_measures, True)
+
+print("The most similar according to SSIM: " , ssim)
+print("The most similar according to RMSE: " , rmse)
+print("The most similar according to SRE: " , sre)
--- improvement.py
+++ improvement.py
@@ -1,6 +1,6 @@
 import os
 import re
-from openai import OpenAI
+import requests
 from dotenv import load_dotenv
 import time
 
@@ -9,56 +9,66 @@ from image import encode_image
 load_dotenv()
 
 # 環境変数を読み込む
-client = OpenAI(
-    api_key=os.environ["OPENAI_API_KEY_V"],
-)
+
+api_key = os.environ['OPENAI_API_KEY_V']
 
 def improve_prompt(origin_image, gen_image, pre_prompt):
     encoded_origin_image = encode_image(origin_image)
     encoded_gen_image = encode_image(gen_image)
+
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {api_key}"
+    }
+
+    payload = {
+        "model": "gpt-4-vision-preview",
+        "messages": [
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "text",
+                        "text": f"The objective is to create the best prompt to generate the first given image in Dalle-3. I generated the second image using Dalle-3 by the prompt {pre_prompt}. First, explain the difference between the two given images. And then, based on the initial prompt and the difference between the generated image and the target image, improve the prompt to generate the first given image in Dalle-3."
+                    },
+                    {
+                        "type": "image_url",
+                        "image_url": {
+                            "url": encoded_origin_image
+                        }
+                    },
+                    {
+                        "type": "image_url",
+                        "image_url": {
+                            "url": encoded_gen_image
+                        }
+                    },
+                    {
+                        "type": "text",
+                        "text": "Generate with the following style: \"diff:\n<diff>\n\nprompt:\n<new prompt>\"",
+                    },
+                    {
+                        "type": "text",
+                        "text": "In the <diff> and <new prompt> sections, output only the contents of the diff and the new prompt.",
+                    }
+                ]
+            }
+        ],
+        "max_tokens": 1000,
+    }
+
     
     while True:
-        response = client.chat.completions.create(
-            model="gpt-4-vision-preview",
-            messages=[
-                {
-                    "role": "user",
-                    "content": [
-                        {"type": "text", "text": f"The objective is to create the best prompt to generate the first given image in Dalle-3. I generated the second image using Dalle-3 by the prompt {pre_prompt}. First, explain the difference between the two given images. And then, based on the initial prompt and the difference between the generated image and the target image, improve the prompt to generate the first given image in Dalle-3."},
-                        {
-                            "type": "image_url",
-                            "image_url": encoded_origin_image,
-                        },
-                        {
-                            "type": "image_url",
-                            "image_url": encoded_gen_image,
-                        },
-                        {
-                            "type": "text",
-                            "text": "Generate with the following style: \"diff:\n<diff>\n\nprompt:\n<new prompt>\"",
-                        },
-                        {
-                            "type": "text",
-                            "text": "In the <diff> and <new prompt> sections, output only the contents of the diff and the new prompt.",
-                        }
-                    ],
-                }
-            ],
-            max_tokens=1000,
-        )
+        response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
 
         if response.status_code == 429:
             print("Rate limit exceeded. Sleeping for a while and retrying...")
             time.sleep(5)  # 5秒待ってからリトライ
             continue
         
-        if response.choices[0].finish_reason != None:
-            print("explain_diff error, finish_reason:", response.choices[0].finish_reason)
-            exit(1)
-
         response.raise_for_status()
         
-        diff, prompt = extract_diff_and_prompt(response.choices[0].message.content)
+        diff, prompt = extract_diff_and_prompt(response.json().get("choices")[0].get("message").get("content"))
         # 両端のクォーテーションマークを削除
         if prompt.startswith('"') and prompt.endswith('"'):
             prompt = prompt[1:-1]
@@ -77,10 +87,10 @@ def extract_diff_and_prompt(text):
 
     return diff, prompt
 
-# if __name__ == "__main__":
-#     pre_prompt = "Photorealistic landscape of a rural scene with a small wooden shack in the foreground, residential houses in the middle distance and a tree-covered hill under a clear blue sky in the background, daytime lighting."
-#     gen_image = "gen_1.jpg"
-#     diff, prompt = improve_prompt("data/image_1/origin_1.jpg", gen_image, pre_prompt)
-#     print(diff)
-#     print("=======================")
-#     print(prompt)
+if __name__ == "__main__":
+    pre_prompt = "Photorealistic landscape of a rural scene with a small wooden shack in the foreground, residential houses in the middle distance and a tree-covered hill under a clear blue sky in the background, daytime lighting."
+    gen_image = "data/image_1/greedy_3/image_1_3.jpg"
+    diff, prompt = improve_prompt("data/image_1/origin_1.jpg", gen_image, pre_prompt)
+    print(diff)
+    print("=======================")
+    print(prompt)
